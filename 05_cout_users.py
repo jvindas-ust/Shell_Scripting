0e1ee5fc72a8d479d901c216a3292ba64e4d6c8b
jvindas-ust <jvindas@ust-global.com>
2019-01-03 09:25:26 -0600

    suggested solution

--- 05_cout_users.py
+++ 05_cout_users.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 import sys
 
-names = {}
+names = {} 
+# {
+# 	jvindas: 3,
+# 	moni: 1
+# }
+
+# TODO: search for "authentication failure" and count the times each user has fail
 for line in sys.stdin.readlines():
 	line = line.strip()
-	# TODO: search for "authentication failure" and count the times each user has fail
+	
+	# line = 'asdkfjhasd asldjfh adf jvindas failure'
+
+	#if line.find('failure') >= 0:
+	#	print(line)
+
+	if 'failure' in line:
+		#cols = line.split() # = ['primera', 'segundas', ... 'n']
+		#cols[3]
+		
+		name = line.split()[3]
+
+		if name in names:
+			# names[name] = names[name] + 1
+			names[name] += 1
+		else:
+			names[name] = 1
+	
+
+print(names)
+
+	
